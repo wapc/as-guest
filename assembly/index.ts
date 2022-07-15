cadd3c40eaacef265ccd1d2e46aaa6c498541470
Phil Kedy <phil.kedy@gmail.com>
2022-07-14 20:00:04 -0400

    Using Result for error handling

--- assembly/index.ts
+++ assembly/index.ts
@@ -23,35 +23,15 @@ export declare function __host_error(ptr: i32): void
 @external("wapc", "__console_log")
 export declare function __console_log(ptr: i32, len: usize): void
 
-const emptyBuffer = new ArrayBuffer(0);
+export type Function = (payload: ArrayBuffer) => Result<ArrayBuffer>
 
-export class Result {
-  readonly buffer: ArrayBuffer
-  readonly error: Error | null
-
-  constructor(buffer: ArrayBuffer, error: Error | null) {
-    this.buffer = buffer
-    this.error = error
-  }
-
-  static ok(buffer: ArrayBuffer): Result {
-    return new Result(buffer, null);
-  }
-
-  static error(err: Error): Result {
-    return new Result(emptyBuffer, err);
-  }
-}
+var functions = new Map<string, Function>();
 
 export class HostError extends Error {
   constructor(message: string) {
     super(message)
   }
 }
-
-export type Function = (payload: ArrayBuffer) => Result
-
-var functions = new Map<string, Function>()
 
 export function register(operation: string, fn: Function): void {
   functions.set(operation, fn)
@@ -64,8 +44,8 @@ function getFunction(name: string): Function {
   return functions.get(name)
 }
 
-function errorFunction(payload: ArrayBuffer): Result {
-  return Result.error(new Error("error"));
+function errorFunction(payload: ArrayBuffer): Result<ArrayBuffer> {
+  return Result.error<ArrayBuffer>(new Error("error"));
 }
 
 export function handleCall(operation_size: usize, payload_size: usize): bool {
@@ -76,18 +56,16 @@ export function handleCall(operation_size: usize, payload_size: usize): bool {
   const operation = String.UTF8.decode(operationBuf)
   const fn = getFunction(operation)
   if (fn != errorFunction) {
-    const result = fn(payload);
-
-    if (result.error) {
-      const errorMsg = result.error!.message;
-      const message = String.UTF8.encode(errorMsg);
-      __guest_error(changetype<i32>(message), message.byteLength);
-      return false;
+    const response = fn(payload)
+    if (response.isOk) {
+      const responseOK = response.get();
+      __guest_response(changetype<i32>(responseOK), responseOK.byteLength)
+      return true
     }
 
-    const response = result.buffer;
-    __guest_response(changetype<i32>(response), response.byteLength)
-    return true
+    const message = String.UTF8.encode(response.error()!.message)
+    __guest_error(changetype<i32>(message), message.byteLength)
+    return false;
   }
 
   const message = String.UTF8.encode("Could not find function \"" + operation + "\"")
@@ -95,7 +73,7 @@ export function handleCall(operation_size: usize, payload_size: usize): bool {
   return false;
 }
 
-export function hostCall(binding: string, namespace: string, operation: string, payload: ArrayBuffer): Result {
+export function hostCall(binding: string, namespace: string, operation: string, payload: ArrayBuffer): Result<ArrayBuffer> {
   const bindingBuf = String.UTF8.encode(binding)
   const namespaceBuf = String.UTF8.encode(namespace)
   const operationBuf = String.UTF8.encode(operation)
@@ -105,18 +83,19 @@ export function hostCall(binding: string, namespace: string, operation: string, 
     changetype<i32>(operationBuf), operationBuf.byteLength,
     changetype<i32>(payload), payload.byteLength)
   if (!result) {
-    const errorLen = __host_error_len();
-    const message = new ArrayBuffer(changetype<i32>(errorLen))
-    __host_error(changetype<i32>(message))
-    const errorMsg = String.UTF8.decode(message)
-    return Result.error(new HostError(errorMsg));
+      const errorLen = __host_error_len();
+      const message = new ArrayBuffer(changetype<i32>(errorLen))
+      __host_error(changetype<i32>(message))
+      const errorMsg = String.UTF8.decode(message);
+
+      return Result.error<ArrayBuffer>(new HostError(errorMsg));
   }
 
   const responseLen = __host_response_len()
   const response = new ArrayBuffer(changetype<i32>(responseLen))
   __host_response(changetype<i32>(response))
 
-  return Result.ok(response);
+  return Result.ok(response)
 }
 
 export function consoleLog(message: string): void {
@@ -129,12 +108,54 @@ export function handleAbort(
   fileName: string | null,
   lineNumber: u32,
   columnNumber: u32
-): void {
-  var errorMessage = (message != null) ? message! : "error occurred"
+): void{
+  var errorMessage = (message!=null) ?message! :"error occurred"
   if (fileName != null && lineNumber != 0 && columnNumber != 0) {
     errorMessage += "; " + fileName! + " (" + lineNumber.toString() + "," + columnNumber.toString() + ")"
   }
   const messageBuf = String.UTF8.encode(errorMessage)
-
+  
   __guest_error(changetype<i32>(messageBuf), messageBuf.byteLength)
+}
+
+export class Result<T> {
+  public readonly isOk: boolean;
+  private readonly ok: T;
+  private readonly err: Error | null;
+
+  static ok<T>(t: T): Result<T> {
+    return new Result<T>(true, t, null);
+  }
+
+  static error<T>(err: Error): Result<T> {
+    return new Result<T>(false, phantomType<T>(), err);
+  }
+
+  protected constructor(isOk: boolean, ok: T, err: Error | null) {
+    this.isOk = isOk;
+    this.ok = ok;
+    this.err = err;
+  }
+
+  get(): T {
+    if (this.isOk) {
+      return this.ok;
+    }
+    throw new Error("called get on Err(E)");
+  }
+
+  error(): Error | null {
+    return this.err;
+  }
+}
+
+function phantomType<T>(): T {
+  if (isInteger<T>()) {
+    return <T>0;
+  } else if (isFloat<T>()) {
+    return <T>0.0;
+  } else {
+    assert(isReference<T>());
+    return changetype<T>(0);
+  }
 }
